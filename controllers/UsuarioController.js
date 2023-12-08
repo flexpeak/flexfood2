f2c83ecffaa9cc9e1606770f4ad74dfde6801f45
Adéliton Fernandes <adeliton@live.com>
2023-12-07 20:43:52 -0400

    Aula hoje

--- controllers/UsuarioController.js
+++ controllers/UsuarioController.js
@@ -1,18 +1,31 @@
 const { usuarios } = require('../models')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 class UsuarioController {
   static async store(req, res) { 
     try {
+
+      const salt = await bcrypt.genSalt(12)
+      const senha = await bcrypt.hash(req.body.senha, salt)
+
       const usuario = await usuarios.create({
         nome: req.body.nome,
         telefone: req.body.telefone,
         endereco: req.body.endereco,
         tipo: req.body.tipo,
-        senha: req.body.senha,
+        senha: senha,
         email: req.body.email
       })
   
-      res.json(usuario)
+      res.json({
+        id: usuario.id,
+        nome: usuario.nome,
+        telefone: usuario.telefone,
+        endereco: usuario.endereco,
+        tipo: usuario.tipo,
+        email: usuario.email
+      })
     } catch(error) {
       console.log(error)
       res.status(500).json({
@@ -20,6 +33,33 @@ class UsuarioController {
       }) 
     } 
     
+  }
+
+  static async login(req, res) {
+
+    const usuario = await usuarios.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+
+    if (usuario) {
+      const match = await bcrypt.compare(req.body.senha, usuario.senha)
+      if (match) {
+        const token = await jwt.sign(usuario.id, '#!@#')
+        res.json({
+          token: token
+        })
+      } else {
+        res.status(401).json({
+          error: "Usuário ou senha inválda"
+        })
+      }
+    } else {
+      res.status(401).json({
+        error: "Usuário ou senha inválida"
+      })
+    }
   }
 }
 
--- routes.js
+++ routes.js
@@ -3,5 +3,6 @@ const UsuarioController = require('./controllers/UsuarioController')
 const router = require('express').Router()
 
 router.post('/usuarios', UsuarioController.store)
+router.post('/usuarios/login', UsuarioController.login)
 
 module.exports = router
